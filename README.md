7ab4fbcee464e0fc640c62485d7f557e6dbb45aa
balibou <benjamin.cherion@gmail.com>
2016-02-14 18:04:41 +0100

    Creating and updating tasks with React

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # meteorReactNative
+
+## Sources
+* [Demo of React Native interacting with a Meteor backend.](https://github.com/spencercarli/meteor-todos-react-native)
+* [Meteor + React Native. Learning from experience.](http://blog.differential.com/meteor-react-native-learning-from-experience/)
---
+++ meteorApp/client/components/App.jsx
@@ -0,0 +1,46 @@
+Meteor.startup(function() {
+  ReactDOM.render(<App /> , document.getElementById('meteorApp'));
+});
+
+App = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}).fetch()
+    }
+  },
+
+  renderTasks() {
+    return this.data.tasks.map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    Tasks.insert({
+      text: text,
+      createdAt: new Date()
+    });
+    ReactDOM.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <form className="new-task" onSubmit={this.handleSubmit} >
+          <input
+            type="text"
+            ref="textInput"
+            placeholder="Type to add new tasks" />
+        </form>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+});
